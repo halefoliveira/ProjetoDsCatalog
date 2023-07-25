5eb12453357cd7a273d6b3990050bcc01a1acb54
Halef Oliveira <halef.alves@gmail.com>
2023-07-25 08:52:56 -0300

    Create entities role e user

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch= FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn (name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -35,9 +36,9 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(Long id, String firsName, String lastName, String email, String password) {
+	public User(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
-		this.firsName = firsName;
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
@@ -51,12 +52,12 @@ public class User implements Serializable{
 		this.id = id;
 	}
 
-	public String getFirsName() {
-		return firsName;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setFirsName(String firsName) {
-		this.firsName = firsName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
 	public String getLastName() {
